1e8be3eacc0ce06e7155c7ca40d77e0c7ace77d9
David Ortiz <z.david.ortiz@gmail.com>
2016-08-26 11:27:56 +0200

    Add runner based on gli

---
+++ lib/xcflushd/runner.rb
@@ -0,0 +1,22 @@
+require 'xcflushd'
+require 'redis'
+require '3scale_client'
+
+module Xcflushd
+  class Runner
+    class << self
+
+      def run(threescale_host, provider_key, redis_host, redis_port)
+        redis = Redis.new(host: redis_host, port: redis_port)
+        threescale = ThreeScale::Client.new(provider_key: provider_key,
+                                            host: threescale_host)
+        reporter = Reporter.new(threescale)
+        authorizer = Authorizer.new(threescale, redis)
+        flusher = Flusher.new(reporter, authorizer, redis)
+
+        flusher.flush
+      end
+
+    end
+  end
+end
