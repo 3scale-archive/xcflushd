34c52dfbe2efddfe32664941da87c86434e7bb45
David Ortiz <z.david.ortiz@gmail.com>
2016-08-30 14:39:25 +0200

    runner: init the Flusher with a storage object instead of a redis client
    Bug fix. The flusher constructor expects a Storage object.

--- lib/xcflushd/runner.rb
+++ lib/xcflushd/runner.rb
@@ -8,11 +8,12 @@ module Xcflushd
 
       def run(threescale_host, provider_key, redis_host, redis_port)
         redis = Redis.new(host: redis_host, port: redis_port)
+        storage = Storage.new(redis)
         threescale = ThreeScale::Client.new(provider_key: provider_key,
                                             host: threescale_host)
         reporter = Reporter.new(threescale)
         authorizer = Authorizer.new(threescale, redis)
-        flusher = Flusher.new(reporter, authorizer, redis)
+        flusher = Flusher.new(reporter, authorizer, storage)
 
         flusher.flush
       end
