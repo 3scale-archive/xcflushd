1a9a19efd049947259d366673cef7236de3bfacd
David Ortiz <z.david.ortiz@gmail.com>
2016-09-06 12:17:35 +0200

    Make threescale backend port configurable

--- lib/xcflushd/runner.rb
+++ lib/xcflushd/runner.rb
@@ -6,11 +6,13 @@ module Xcflushd
   class Runner
     class << self
 
-      def run(threescale_host, provider_key, redis_host, redis_port, auth_valid_min)
+      def run(threescale_host, threescale_port, provider_key,
+              redis_host, redis_port, auth_valid_min)
         redis = Redis.new(host: redis_host, port: redis_port, driver: :hiredis)
         storage = Storage.new(redis)
         threescale = ThreeScale::Client.new(provider_key: provider_key,
-                                            host: threescale_host)
+                                            host: threescale_host,
+                                            port: threescale_port)
         reporter = Reporter.new(threescale)
         authorizer = Authorizer.new(threescale, redis, auth_valid_min)
         flusher = Flusher.new(reporter, authorizer, storage)
