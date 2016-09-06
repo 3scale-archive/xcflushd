2449c56ffdb1cf9f8018003df9d0419207a0796c
David Ortiz <z.david.ortiz@gmail.com>
2016-09-06 18:41:47 +0200

    authorizer: renew auths of non-limited metrics

--- spec/authorizer_spec.rb
+++ spec/authorizer_spec.rb
@@ -17,6 +17,7 @@ module Xcflushd
       let(:user_key) { 'a_user_key' }
       let(:auth_hash_key) { subject.send(:auth_hash_key, service_id, user_key) }
       let(:metric) { 'a_metric' }
+      let(:reported_metrics) { [metric] }
 
       before do
         test_report_usages = app_report_usages
@@ -32,12 +33,12 @@ module Xcflushd
 
       shared_examples 'app with metric above the usage limit' do
         it 'marks the metric as non-authorized' do
-          subject.renew_authorizations(service_id, user_key)
+          subject.renew_authorizations(service_id, user_key, reported_metrics)
           expect(redis.hget(auth_hash_key, metric)).to eq '0'
         end
 
         it 'sets a ttl for the hash key of the application in the storage' do
-          subject.renew_authorizations(service_id, user_key)
+          subject.renew_authorizations(service_id, user_key, reported_metrics)
           expect(redis.ttl(auth_hash_key)).to be_between(0, auths_valid_min*60)
         end
       end
@@ -72,19 +73,20 @@ module Xcflushd
         end
 
         it 'marks the metric as authorized' do
-          subject.renew_authorizations(service_id, user_key)
+          subject.renew_authorizations(service_id, user_key, reported_metrics)
           expect(redis.hget(auth_hash_key, metric)).to eq '1'
         end
 
         it 'sets a ttl for the hash key of the application in the storage' do
-          subject.renew_authorizations(service_id, user_key)
+          subject.renew_authorizations(service_id, user_key, reported_metrics)
           expect(redis.ttl(auth_hash_key)).to be_between(0, auths_valid_min*60)
         end
       end
 
-      context 'when the app has several metrics' do
+      context 'when the app has several limited metrics' do
         let(:authorized_metrics) { %w(am1 am2) }
         let(:unauthorized_metrics) { %w(um1 um2) }
+        let(:reported_metrics) { authorized_metrics + unauthorized_metrics }
 
         let(:non_exceeded_report_usages) do
           authorized_metrics.map { |metric| Usage.new(metric, 'hour', 1, 2) }
@@ -99,7 +101,7 @@ module Xcflushd
         end
 
         it 'renews the authorization for all of them' do
-          subject.renew_authorizations(service_id, user_key)
+          subject.renew_authorizations(service_id, user_key, reported_metrics)
 
           authorized_metrics.each do |metric|
             expect(redis.hget(auth_hash_key, metric)).to eq '1'
@@ -111,13 +113,58 @@ module Xcflushd
         end
 
         it 'sets a ttl for the hash key of the application in the storage' do
-          subject.renew_authorizations(service_id, user_key)
+          subject.renew_authorizations(service_id, user_key, reported_metrics)
           expect(redis.ttl(auth_hash_key)).to be_between(0, auths_valid_min*60)
         end
       end
 
-      # TODO: test case of non-limited metrics. Investigate what the
-      # 3scale_client returns in that case.
+      context 'when there is a non-limited metric that has been reported' do
+        let(:non_limited_metric) { 'a_non_limited_metric' }
+        let(:reported_metrics) { [non_limited_metric] }
+
+        let(:app_report_usages) { [] } # Only limited metrics have reports
+
+        before do
+          allow(threescale_client)
+              .to receive(:authorize)
+              .with({ service_id: service_id,
+                      user_key: user_key,
+                      usage: { non_limited_metric => 1 } })
+              .and_return(authorize_resp)
+        end
+
+        context 'and the metric is authorized' do
+          let(:authorize_resp) do
+            Object.new.tap { |o| o.define_singleton_method(:success?) { true } }
+          end
+
+          it 'marks the metric as authorized' do
+            subject.renew_authorizations(service_id, user_key, reported_metrics)
+            expect(redis.hget(auth_hash_key, non_limited_metric)).to eq '1'
+          end
+
+          it 'sets a ttl for the hash key of the application in the storage' do
+            subject.renew_authorizations(service_id, user_key, reported_metrics)
+            expect(redis.ttl(auth_hash_key)).to be_between(0, auths_valid_min*60)
+          end
+        end
+
+        context 'and the metric is not authorized' do
+          let(:authorize_resp) do
+            Object.new.tap { |o| o.define_singleton_method(:success?) { false } }
+          end
+
+          it 'marks the metric as non-authorized' do
+            subject.renew_authorizations(service_id, user_key, reported_metrics)
+            expect(redis.hget(auth_hash_key, non_limited_metric)).to eq '0'
+          end
+
+          it 'sets a ttl for the hash key of the application in the storage' do
+            subject.renew_authorizations(service_id, user_key, reported_metrics)
+            expect(redis.ttl(auth_hash_key)).to be_between(0, auths_valid_min*60)
+          end
+        end
+      end
     end
   end
 end
