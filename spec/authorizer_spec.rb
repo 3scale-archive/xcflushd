d4b65c83bc36b3a23f749b473601a9b6eb3f0abd
David Ortiz <z.david.ortiz@gmail.com>
2016-09-23 16:31:32 +0200

    spec/authorizer: fix mocking for usage limits exceeded cases
    When a usage limit is exceeded, the response coming from the 3scale client
    responds to #success? and also to #error_code and #error_message. This was
    not being mocked correctly.
    
    This commit also simplifies some things by using shared_examples and defining
    global lets.

--- spec/authorizer_spec.rb
+++ spec/authorizer_spec.rb
@@ -16,24 +16,37 @@ module Xcflushd
       let(:metric) { 'a_metric' }
       let(:reported_metrics) { [metric] }
 
-      before do
-        test_report_usages = app_report_usages
-        authorize_resp = Object.new.tap do |o|
-          o.define_singleton_method(:success?) { true }
-          o.define_singleton_method(:usage_reports) { test_report_usages }
-        end
+      # Default authorized response. Overwritten in cases where auth is denied.
+      let(:authorize_response) do
+        double('auth_response',
+               success?: true,
+               error_code: nil,
+               error_message: nil,
+               usage_reports: app_report_usages)
+      end
 
+      let(:denied_auth_response_limits_exceeded) do
+        double('auth_response',
+               success?: false,
+               error_code: 'limits_exceeded',
+               error_message: 'usage limits are exceeded',
+               usage_reports: app_report_usages)
+      end
+
+      before do
         allow(threescale_client)
             .to receive(:authorize)
             .with({ service_id: service_id, user_key: user_key })
-            .and_return(authorize_resp)
+            .and_return(authorize_response)
       end
 
-      shared_examples 'app with non-authorized metric' do
-        it 'returns a denied authorization' do
+      # These shared_examples only apply when there only 1 metric is reported
+      shared_examples 'denied auth' do |reason|
+        it 'returns a denied authorization that includes the reason' do
           auth = subject.authorizations(service_id, user_key, reported_metrics).first
           expect(auth.metric).to eq metric
           expect(auth.authorized?).to be false
+          expect(auth.reason).to eq reason
         end
       end
 
@@ -45,28 +58,33 @@ module Xcflushd
         end
       end
 
-      context 'when a metric has a usage in any period that is the same as the limit' do
-        let(:app_report_usages) do
-          [Usage.new(metric, 'hour', 1, 1), Usage.new(metric, 'day', 1, 2)]
+      context 'when the authorization is denied because of usage limits exceeded' do
+        let(:authorize_response) { denied_auth_response_limits_exceeded }
+        reason = 'limits_exceeded'
+
+        context 'because the usage in any period that is the same as the limit' do
+          let(:app_report_usages) do
+            [Usage.new(metric, 'hour', 1, 1), Usage.new(metric, 'day', 1, 2)]
+          end
+
+          include_examples 'denied auth', reason
         end
 
-        include_examples 'app with non-authorized metric'
-      end
+        context 'because the usage is above the limit in any period' do
+          let(:app_report_usages) do
+            [Usage.new(metric, 'hour', 2, 1), Usage.new(metric, 'day', 1, 2)]
+          end
 
-      context 'when a metric has a usage above the limit in any period' do
-        let(:app_report_usages) do
-          [Usage.new(metric, 'hour', 2, 1), Usage.new(metric, 'day', 1, 2)]
+          include_examples 'denied auth', reason
         end
 
-        include_examples 'app with non-authorized metric'
-      end
+        context 'because the usage is above the limits for all the periods' do
+          let(:app_report_usages) do
+            [Usage.new(metric, 'hour', 2, 1), Usage.new(metric, 'day', 2, 1)]
+          end
 
-      context 'when a metric is above the limits for all the periods' do
-        let(:app_report_usages) do
-          [Usage.new(metric, 'hour', 2, 1), Usage.new(metric, 'day', 2, 1)]
+          include_examples 'denied auth', reason
         end
-
-        include_examples 'app with non-authorized metric'
       end
 
       context 'when a metric is below the limits for all the periods' do
@@ -77,7 +95,7 @@ module Xcflushd
         include_examples 'app with authorized metric'
       end
 
-      context 'when the app has several limited metrics' do
+      context 'when the app has several limited metrics, some authorized and others not' do
         let(:authorized_metrics) { %w(am1 am2) }
         let(:unauthorized_metrics) { %w(um1 um2) }
         let(:reported_metrics) { authorized_metrics + unauthorized_metrics }
@@ -94,6 +112,8 @@ module Xcflushd
           non_exceeded_report_usages + exceeded_report_usages
         end
 
+        let(:authorize_response) { denied_auth_response_limits_exceeded }
+
         it 'returns the correct authorization status for all of them' do
           auths = subject.authorizations(service_id, user_key, reported_metrics)
 
@@ -103,14 +123,14 @@ module Xcflushd
           end
 
           unauthorized_metrics.each do |metric|
-            expect(auths.any? { |auth| auth.metric == metric && !auth.authorized? })
-                .to be true
+            expect(auths.any? do |auth|
+              auth.metric == metric && !auth.authorized? && auth.reason == 'limits_exceeded'
+            end).to be true
           end
         end
       end
 
       context 'when there is a non-limited metric that has been reported' do
-        let(:metric) { 'a_non_limited_metric' }
         let(:reported_metrics) { [metric] }
         let(:app_report_usages) { [] } # Only limited metrics have reports
 
@@ -120,38 +140,35 @@ module Xcflushd
               .with({ service_id: service_id,
                       user_key: user_key,
                       usage: { metric => 1 } })
-              .and_return(authorize_resp)
+              .and_return(authorize_response)
         end
 
         context 'and the metric is authorized' do
-          let(:authorize_resp) do
-            Object.new.tap { |o| o.define_singleton_method(:success?) { true } }
-          end
-
           include_examples 'app with authorized metric'
         end
 
-        context 'and the metric is not authorized' do
-          let(:authorize_resp) do
-            Object.new.tap do |o|
-              o.define_singleton_method(:success?) { false }
-              o.define_singleton_method(:error_code) { 'a_deny_reason' }
-            end
+        context 'and the metric is not authorized because of usage limits exceeded' do
+          reason = 'limits_exceeded'
+          let(:authorize_response) { denied_auth_response_limits_exceeded }
+          include_examples 'denied auth', reason
+        end
+
+        context 'and the metric is not authorized for a reason other than limits exceeded' do
+          reason = 'some_reason'
+          let(:authorize_response) do
+            double('auth_response',
+                   success?: false, error_code: reason, error_message: 'msg')
           end
 
-          include_examples 'app with non-authorized metric'
+          include_examples 'denied auth', reason
         end
       end
 
-      context 'when there is a disable metric' do
+      context 'when there is a disabled metric' do
+        reason = described_class.const_get(:DISABLED_METRIC)
         let(:app_report_usages) { [Usage.new(metric, 'hour', 0, 0)] }
 
-        it 'returns a denied authorization that includes the reason' do
-          auth = subject.authorizations(service_id, user_key, reported_metrics).first
-          expect(auth.metric).to eq metric
-          expect(auth.authorized?).to be false
-          expect(auth.reason).to eq described_class.const_get(:DISABLED_METRIC)
-        end
+        include_examples 'denied auth', reason
       end
 
       context 'when the authorization is denied and it is not because limits are exceeded' do
