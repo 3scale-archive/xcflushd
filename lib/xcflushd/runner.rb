6397c001d698e46bf14cccee58556a6972e7cf58
David Ortiz <z.david.ortiz@gmail.com>
2016-09-27 12:57:56 +0200

    runner: initialize the priority auth renewer correctly
    Solves a silly bug. It's calling initialize using a var that has not been
    initialized.

--- lib/xcflushd/runner.rb
+++ lib/xcflushd/runner.rb
@@ -27,7 +27,8 @@ module Xcflushd
               host: opts[:redis_host], port: opts[:redis_port], driver: :hiredis)
           redis_sub = Redis.new(
               host: opts[:redis_host], port: opts[:redis_port], driver: :hiredis)
-          PriorityAuthRenewer.new(authorizer, storage, redis_pub, redis_sub, auth_valid_min)
+          PriorityAuthRenewer.new(
+              authorizer, storage, redis_pub, redis_sub, opts[:auth_valid_minutes])
         end
 
         flush_periodically(flusher, opts[:reporting_freq_minutes], logger)
