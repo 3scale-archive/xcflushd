8b862a87b03439bfad333f0a30753ca53c9856d7
Alejandro Martinez Ruiz <alex@flawedcode.org>
2016-10-25 12:31:10 +0200

    authorization: new interface as a struct without metric names
    This is meant to decouple the authorization status from the metric
    names and avoid allocating objects that provide next to no valuable
    data. That's why we now have the self.ok! and self.denied! methods,
    which will try to return you a singleton instance of a status.

--- lib/xcflushd/authorization.rb
+++ lib/xcflushd/authorization.rb
@@ -1,17 +1,45 @@
 module Xcflushd
-  class Authorization
-
-    attr_reader :metric, :reason
-
-    def initialize(metric, authorized, reason = nil)
-      @metric = metric
-      @authorized = authorized
-      @reason = authorized ? nil : reason
+  Authorization = Struct.new(:authorized, :reason) do
+    def initialize(authorized, reason = nil)
+      super(authorized, authorized ? nil : reason)
     end
 
     def authorized?
-      @authorized
+      authorized
+    end
+  end
+
+  class Authorization
+    # This is inevitably tied to the 3scale backend code
+    LIMITS_EXCEEDED_CODE = 'limits_exceeded'.freeze
+    private_constant :LIMITS_EXCEEDED_CODE
+
+    AUTHORIZED = new(true).freeze
+    private_constant :AUTHORIZED
+    DENIED = new(false).freeze
+    private_constant :DENIED
+    LIMITS_EXCEEDED = new(false, LIMITS_EXCEEDED_CODE).freeze
+    private_constant :LIMITS_EXCEEDED
+
+    private_class_method :new
+
+    def limits_exceeded?
+      reason == LIMITS_EXCEEDED_CODE
     end
 
+    def self.ok!
+      AUTHORIZED
+    end
+
+    def self.denied!(reason = nil)
+      if reason.nil?
+        DENIED
+      # this test has to be done in case the code changes
+      elsif reason == LIMITS_EXCEEDED_CODE
+        LIMITS_EXCEEDED
+      else
+        new(false, reason)
+      end
+    end
   end
 end
